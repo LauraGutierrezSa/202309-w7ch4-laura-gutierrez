a0f4d9f3f1bff880f35284ce4539d3e7a3f8b2cf
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-26 18:34:40 +0200

    Add initial structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,7 @@
-const apiUrl = import.meta.env.VITE_SLOTHAPI_URL;
+export const apiUrl = import.meta.env.VITE_SLOTHAPI_URL;
 
-export default apiUrl;
+const App = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export default App;
