2e9704ca23d4fed19c88e1ca32a80dda1174a84d
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-27 07:24:12 +0200

    Add sloth form path to App component

---
+++ src/App/App.tsx
@@ -0,0 +1,7 @@
+export const apiUrl = import.meta.env.VITE_SLOTHAPI_URL;
+
+const App = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export default App;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
+import SlothForm from "../slothForm/slothForm";
+
 const App = (): React.ReactElement => {
   return (
     <div className="main-container">
       <header className="main-header">
         <h1 className="main-title"> 🦥 Creador de perezosos 🦥 </h1>
       </header>
+      <SlothForm />
     </div>
   );
 };
